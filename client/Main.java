2ced1589940b37b8b295231d7a14c6fb0518ddc2
hyper <hyperskill@hyperskill.org>
2023-12-25 19:35:35 +0600

    3/4 Stage

--- client/Main.java
+++ client/Main.java
@@ -5,6 +5,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Main {
 
@@ -19,12 +20,65 @@ public class Main {
                 DataOutputStream output = new DataOutputStream(socket.getOutputStream());
             )
         {
-            System.out.println("Client started!");
-            String wordsToServer = "Give me everything you have!";
-            output.writeUTF(wordsToServer);
-            System.out.println("Sent: " + wordsToServer);
-            String wordsFromServer = input.readUTF();
-            System.out.println("Received: " + wordsFromServer);
+            //System.out.println("Client started!");
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter action (1 - get a file, 2 - create a file, 3 - delete a file): ");
+            String actionOption = scanner.nextLine();
+            switch (actionOption) {
+                case "1":
+                    System.out.print("Enter filename: ");
+                    String fileName2 = scanner.nextLine();
+                    output.writeUTF("GET " + fileName2);
+
+                    System.out.println("The request was sent.");
+                    String serverResponseMsg = input.readUTF();
+
+                    String[] arrServerResponseMsg = serverResponseMsg.split(" ");
+                    switch (arrServerResponseMsg[0]) {
+                        case "200":
+                            System.out.println("The content of the file is: " + arrServerResponseMsg[1]);
+                            break;
+                        case "404":
+                            System.out.println("The response says that the file was not found!");
+                            break;
+                    }
+
+                    break;
+                case "2":
+                    System.out.print("Enter filename: ");
+                    String fileName = scanner.nextLine();
+                    System.out.print("Enter file content ");
+                    String fileContent = scanner.nextLine();
+                    output.writeUTF("PUT " + fileName + " " + fileContent);
+                    System.out.println("The request was sent.");
+                    String responseMsg = input.readUTF();
+                    switch (responseMsg) {
+                        case "200":
+                            System.out.println("The response says that file was created!");
+                            break;
+                        case "403":
+                            System.out.println("The response says that creating the file was forbidden!");
+                            break;
+                    }
+                    break;
+                case "3":
+                    System.out.print("Enter filename: ");
+                    String fileNameForDelete = scanner.nextLine();
+                    output.writeUTF("DELETE " + fileNameForDelete);
+                    System.out.println("The request was sent.");
+                    String responseResult = input.readUTF();
+                    switch (responseResult) {
+                        case "200":
+                            System.out.println("The response says that the file was successfully deleted!");
+                            break;
+                        case "404":
+                            System.out.println("The response says that the file was not found!");
+                            break;
+                    }
+                    break;
+                case "exit":
+                    output.writeUTF("exit");
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
--- server/Main.java
+++ server/Main.java
@@ -1,35 +1,74 @@
 package server;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Main {
 
     private static final String SERVER_ADDRESS = "127.0.0.1";
     private static final int PORT = 23456;
+    private static final String DATA_DIR =
+            System.getProperty("user.dir") + "/src/server/data/";
 
     public static void main(String[] args) {
 
-        System.out.println("Server started!");
-        try (
-                ServerSocket serverSocket = new ServerSocket(PORT, 50, InetAddress.getByName(SERVER_ADDRESS));
-                Socket socket = serverSocket.accept();
-            )
-        {
-            DataInputStream input = new DataInputStream(socket.getInputStream());
-            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
+        //System.out.println("Server started!");
+        while (true) {
+            try (
+                    ServerSocket serverSocket = new ServerSocket(PORT, 50, InetAddress.getByName(SERVER_ADDRESS));
+                    Socket socket = serverSocket.accept();
+            ) {
+                DataInputStream input = new DataInputStream(socket.getInputStream());
+                DataOutputStream output = new DataOutputStream(socket.getOutputStream());
 
-            String wordsFromClient = input.readUTF();
-            System.out.println("Received: " + wordsFromClient);
-            String wordsToClient = "All files were sent!";
-            output.writeUTF(wordsToClient);
-            System.out.println("Sent: " + wordsToClient);
-        } catch (IOException e) {
-            e.printStackTrace();
+                String clientMsg = input.readUTF();
+                if (clientMsg.equals("exit")) {
+                    return;
+                }
+                String[] arrClientMsg = clientMsg.split(" ");
+                switch (arrClientMsg[0]) {
+                    case "GET":
+                        File f2 = new File(DATA_DIR + arrClientMsg[1]);
+                        if (f2.exists() && !f2.isDirectory()) {
+                            String fileContent = Files.readString(Paths.get(DATA_DIR + arrClientMsg[1]));
+                            output.writeUTF("200 " + fileContent);
+                        } else {
+                            output.writeUTF("404");
+                        }
+                        break;
+
+                        case "PUT":
+                        File f = new File(DATA_DIR + arrClientMsg[1]);
+                        if (f.exists() && !f.isDirectory()) {
+                            output.writeUTF("403");
+                        } else {
+                            try (Writer writer = new BufferedWriter(new OutputStreamWriter(
+                                    new FileOutputStream(DATA_DIR + arrClientMsg[1]), "utf-8"))) {
+                                writer.write(arrClientMsg[2]);
+                            }
+                            output.writeUTF("200");
+                        }
+                        break;
+
+                        case "DELETE":
+                        File f3 = new File(DATA_DIR + arrClientMsg[1]);
+                        if (f3.exists() && !f3.isDirectory()) {
+                            f3.delete();
+                            output.writeUTF("200");
+                        } else {
+                            output.writeUTF("404");
+                        }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
